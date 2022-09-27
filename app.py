343860f4d13591e8e43b0694a54dcb0c2e8c326d
Artur <artur.prado@gmail.com>
2022-09-27 20:14:32 -0300

    Fix non-ascii error when running the game

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+# -*- coding: utf-8 -*-
+# This magic commend above solves the error that is raising on MacOS when trying
+# to execute the game:
+#
+#  File "app.py", line 26
+#  SyntaxError: Non-ASCII character '\xc3' in file app.py on line 26, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
+
 import pygame, sys
 from pygame.locals import *
 import random, time
- 
+
 pygame.init()
- 
+
 ### Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
- 
+
 ### Creating colors
 BLACK = (0, 0, 0)
 GRAY = (103, 103, 143)
 WHITE = (255, 255, 255)
- 
+
 #Other Variables for use in the program
 SCREEN_WIDTH = 550
 SCREEN_HEIGHT = 450
@@ -20,7 +27,7 @@ SPEED = 0
 SCORE = 0
 START = 0
 LIFES = 3
- 
+
 ### Setting up Fonts
 font = pygame.font.SysFont("Verdana", 20)
 start_instruction = font.render("Aperte Espaço para começar...", True, GRAY)
@@ -31,7 +38,7 @@ game_over = pygame.image.load("./resources/images/gameover.png")
 
 image_not_scaled = pygame.image.load("./resources/images/GroceryShelf.png")
 background = pygame.transform.scale(image_not_scaled, (SCREEN_WIDTH, SCREEN_HEIGHT))
- 
+
 ### Create a white screen
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
@@ -46,13 +53,13 @@ vegan_products_list = ["./resources/images/onion.png", "./resources/images/eggpl
 
 class VeganProducts(pygame.sprite.Sprite):
       def __init__(self):
-        super().__init__() 
+        super().__init__()
 
         self.update()
 
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(30,SCREEN_WIDTH-30),-30) 
- 
+        self.rect.center = (random.randint(30,SCREEN_WIDTH-30),-30)
+
       def move(self):
         self.rect.move_ip(0, SPEED)
         if (self.rect.top > SCREEN_HEIGHT):
@@ -68,7 +75,7 @@ class VeganProducts(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(image_not_scaled, (64, 64))
 
       def draw(self, surface):
-        surface.blit(self.image, self.rect) 
+        surface.blit(self.image, self.rect)
 
 
 enemies_list = ["./resources/images/beef.png", "./resources/images/cheese.png", "./resources/images/chicken.png"]
@@ -81,7 +88,7 @@ class Enemies(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(30, SCREEN_WIDTH-30), 0)
-    
+
     def move(self):
         self.rect.move_ip(0, SPEED)
         if (self.rect.top > SCREEN_HEIGHT):
@@ -101,26 +108,26 @@ class Enemies(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.image = pygame.image.load("./resources/images/Basket.png")
-        
+
         self.rect = self.image.get_rect()
         self.rect.center = (160, 400)
- 
+
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-         
+
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:        
+        if self.rect.right < SCREEN_WIDTH:
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
- 
+
     def draw(self, surface):
-        surface.blit(self.image, self.rect)     
- 
-### Setting up Sprites   
+        surface.blit(self.image, self.rect)
+
+### Setting up Sprites
 P1 = Player()
 VP1 = VeganProducts()
 VP2 = VeganProducts()
@@ -139,12 +146,12 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(VP1)
 
-### Adding a new User event 
+### Adding a new User event
 INC_SPEED = pygame.USEREVENT + 1       # Makes the event unique
 pygame.time.set_timer(INC_SPEED, 1000) # Fires the event every 1000 ms (this will increase the speed later in the code)
 
 
-pygame.mixer.music.load('./resources/audio/background.wav') 
+pygame.mixer.music.load('./resources/audio/background.wav')
 pygame.mixer.music.play(-1)  # -1 makes music play in a loop
 
 events = pygame.event.get()
@@ -153,13 +160,13 @@ for event in events:
         if event.key == pygame.K_SPACE:
             START = 1
 
-while True:     
+while True:
 
-    ### Cycles through all events occuring  
+    ### Cycles through all events occuring
     for event in pygame.event.get():
-            
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and START == 0: 
+            if event.key == pygame.K_SPACE and START == 0:
                 START = 1
                 SPEED = 5
 
@@ -186,7 +193,7 @@ while True:
     if SCORE == 20:
         veganProducts.add(VP2)
         all_sprites.add(VP2)
-    
+
     if SCORE == 30:
         enemies.add(E1)
         all_sprites.add(E1)
@@ -204,21 +211,21 @@ while True:
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
-    
+
     ### To be run if collision occurs between Player and VeganProducts
     if pygame.sprite.spritecollideany(P1, veganProducts):
- 
-        pygame.sprite.spritecollideany(P1, veganProducts).update()   
-        pygame.sprite.spritecollideany(P1, veganProducts).reappear()   
 
-        SCORE += 1 
+        pygame.sprite.spritecollideany(P1, veganProducts).update()
+        pygame.sprite.spritecollideany(P1, veganProducts).reappear()
+
+        SCORE += 1
 
     ### To be run if collision occurs between Player and Enemies
     if pygame.sprite.spritecollideany(P1, enemies):
         pygame.mixer.Sound('./resources/audio/fail.wav').play()
 
-        pygame.sprite.spritecollideany(P1, enemies).update()   
-        pygame.sprite.spritecollideany(P1, enemies).reappear() 
+        pygame.sprite.spritecollideany(P1, enemies).update()
+        pygame.sprite.spritecollideany(P1, enemies).reappear()
 
         LIFES -= 1
 
@@ -233,13 +240,13 @@ while True:
         pygame.display.update()
 
         for entity in all_sprites:
-            entity.kill() 
+            entity.kill()
 
         print(SCORE)
 
         time.sleep(3)
         pygame.quit()
-        sys.exit()  
-        
+        sys.exit()
+
     pygame.display.update()
     FramePerSec.tick(FPS)
